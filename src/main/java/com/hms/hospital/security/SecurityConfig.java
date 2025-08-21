036b76f7e55654b65b47078d9e4afabb67a960ad
KrishnaSChavan1 <KrishnaS.Chavan@cognizant.com>
2025-08-21 10:01:18 +0530

    all auth

--- src/main/java/com/hms/hospital/security/SecurityConfig.java
+++ src/main/java/com/hms/hospital/security/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.hms.hospital.security;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -34,7 +36,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
                         .requestMatchers("/login", "/register", "/users/save").permitAll() // allow without login
-//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/doctors/**").hasRole("DOCTOR")
                         .requestMatchers("/patient/**").hasRole("PATIENT")
 //                        .requestMatchers("/users/**").permitAll()
